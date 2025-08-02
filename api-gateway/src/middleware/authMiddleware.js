60e5dc6bd1ec836c82b8aedfd3f7f82dcfb2717d
rtom03 <zuckm709@gmail.com>
2025-08-02 16:54:18 +0100

    implement READ DEL endpoint(post-service)

--- api-gateway/src/middleware/authMiddleware.js
+++ api-gateway/src/middleware/authMiddleware.js
@@ -3,14 +3,14 @@ import jwt from "jsonwebtoken";
 export const validateToken = (req, res, next) => {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
-  console.log(token);
+  // console.log(token);
   if (!token) {
     return res.status(401).json({ success: false, message: "Invalid token" });
   }
 
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    console.log("Using secret:", process.env.JWT_SECRET);
-    console.log("Decoded user:", user);
+    // console.log("Using secret:", process.env.JWT_SECRET);
+    // console.log("Decoded user:", user);
     if (err) {
       console.log(err);
       return res.status(429).json({
